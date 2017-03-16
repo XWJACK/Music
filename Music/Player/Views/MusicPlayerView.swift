a3f1984e4141847cee6eca8a7750c8f3ca17ba1a
Jack <xuwenjiejack@gmail.com>
2017-03-16 16:35:15 +0800

    Add New MusicPlayerView

---
+++ Music/Player/Views/MusicPlayerView.swift
@@ -0,0 +1,21 @@
+//
+//  MusicPlayerView.swift
+//  Music
+//
+//  Created by Jack on 3/16/17.
+//  Copyright © 2017 Jack. All rights reserved.
+//
+
+import UIKit
+
+class MusicPlayerView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}

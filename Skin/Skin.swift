474cb67773e78853465d7a8e46a4be8e5bf4f569
Jack <xuwenjiejack@gmail.com>
2017-03-17 09:58:04 +0800

    Remove Skin to Framework

--- Skin/Skin.swift
+++ Skin/Skin.swift
@@ -2,7 +2,7 @@
 //  Skin.swift
 //  Music
 //
-//  Created by Jack on 3/15/17.
+//  Created by Jack on 3/17/17.
 //  Copyright © 2017 Jack. All rights reserved.
 //
 
@@ -10,6 +10,21 @@ import UIKit
 
 extension Notification.Name {
     static let ChangeSkin: Notification.Name = Notification.Name.init("ChangeSkin")
+}
+
+@objc protocol Skin {
+    func addSkin()
+    func removeSkin()
+    @objc func updateSkin()
+}
+
+extension Skin where Self: UIView {
+    func addSkin() {
+        NotificationCenter.default.addObserver(self, selector: #selector(updateSkin), name: .ChangeSkin, object: nil)
+    }
+    func removeSkin() {
+        NotificationCenter.default.removeObserver(self, name: .ChangeSkin, object: nil)
+    }
 }
 
 class SkinManager {
@@ -25,8 +40,4 @@ class SkinManager {
     func changeSkin(by index: Int) {
         NotificationCenter.default.post(name: .ChangeSkin, object: nil, userInfo: nil)
     }
-}
-
-struct Skin {
-    static var tab1Image: UIImage? { return UIImage(contentsOfFile: "") }
 }

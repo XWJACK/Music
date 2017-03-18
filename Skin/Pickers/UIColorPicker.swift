967e2778fc7beb829c4bae40003f86f2199aa646
Jack <xuwenjiejack@gmail.com>
2017-03-18 23:49:18 +0800

    Add New File for Skin

---
+++ Skin/Pickers/UIColorPicker.swift
@@ -0,0 +1,9 @@
+//
+//  UIColorPicker.swift
+//  Music
+//
+//  Created by Jack on 3/17/17.
+//  Copyright © 2017 Jack. All rights reserved.
+//
+
+import Foundation
---
+++ Skin/UIKit/UIViewSkin.swift
@@ -0,0 +1,9 @@
+//
+//  UIViewSkin.swift
+//  Music
+//
+//  Created by Jack on 3/17/17.
+//  Copyright © 2017 Jack. All rights reserved.
+//
+
+import Foundation
